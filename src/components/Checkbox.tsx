f5a90765b65dbc41d647ea16227c1848d4c305bb
Tavinder Singh <tavinder123singh@gmail.com>
2024-07-11 16:11:37 +0530

    removed border on click

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -8,9 +8,13 @@ export default function Checkbox() {
   return (
     <div className="flex">
       <div
-        className={`flex group h-[25px] w-[25px] rounded-md border-[#CDCDCD] border cursor-pointer items-center justify-center hover:border-[#BDBDBD] ${
+        className={`flex group h-[25px] w-[25px] rounded-md cursor-pointer items-center justify-center hover:border-[#BDBDBD] ${
           isPressing ? "ring-2 ring-[#91b2fe]" : ""
-        } ${isSelected ? "bg-[#2469F6] hover:bg-[#5087F8]" : "bg-white"}`}
+        } ${
+          isSelected
+            ? "bg-[#2469F6] hover:bg-[#5087F8]"
+            : "bg-white border-[#CDCDCD] border"
+        }`}
         onMouseDown={() => setIsPressing(true)}
         onMouseUp={() => setIsPressing(false)}
         onClick={() => setIsSelected(!isSelected)}
